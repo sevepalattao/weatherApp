c5553ae1b8432c867421dbc7b715dd355ba57770
BigSev15 <palat008@umn.edu>
2022-10-10 18:02:02 -0500

    added loop that creates card elements with the 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,28 @@ function getWeather (lat, lon) {
       })
       .then(function(data) {
         console.log(data);
+        var deck = $(".card-deck");
+        deck.empty();
         $("#temp").text('Temp: ' + data.current.temp + '°F');
         $("#wind").text("Wind: " + data.current.wind_speed + 'MPH');
         $("#humid").text("Humidity: " + data.current.humidity + '%');
+        $("#5day-header").text("5-day Forecast: ");
+        for (i=1; i < 6; i++) {
+            var forecast = $("<div class='card text-white bg-primary mb-3 d-inline-block'>");
+            var temperature = $("<p>");
+            var windy = $("<p>");
+            var humidi = $("<p>");
+            var date = $("<h5>");
+            date.text(moment.unix(data.daily[i].dt).format("dddd MM/DD/YYYY"));
+            temperature.text("Temp: " + data.daily[i].temp.day + '°F');
+            windy.text("Wind: " + data.daily[i].wind_speed + 'MPH');
+            humidi.text("Humidity: " + data.daily[i].humidity + '%');
+            forecast.append(date);
+            forecast.append(temperature);
+            forecast.append(windy);
+            forecast.append(humidi);
+            deck.append(forecast);
+        }
       });
 }
 
