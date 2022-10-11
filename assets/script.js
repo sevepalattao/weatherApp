fd16a39822ea192a81f040e7697a09640ce12fca
BigSev15 <palat008@umn.edu>
2022-10-11 16:29:08 -0500

    added icons, local storage functionality, and made history buttons present weather for that city

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,6 @@ function getWeather (lat, lon) {
         return response.json();
       })
       .then(function(data) {
-        console.log(data);
         var deck = $(".card-deck");
         deck.empty();
         $("#temp").text('Temp: ' + data.current.temp + '°F');
@@ -18,11 +17,14 @@ function getWeather (lat, lon) {
             var windy = $("<p>");
             var humidi = $("<p>");
             var date = $("<h5>");
+            var icon = $("<img>");
             date.text(moment.unix(data.daily[i].dt).format("dddd MM/DD/YYYY"));
+            icon.attr("src", "https://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png");
             temperature.text("Temp: " + data.daily[i].temp.day + '°F');
             windy.text("Wind: " + data.daily[i].wind_speed + 'MPH');
             humidi.text("Humidity: " + data.daily[i].humidity + '%');
             forecast.append(date);
+            forecast.append(icon);
             forecast.append(temperature);
             forecast.append(windy);
             forecast.append(humidi);
@@ -41,15 +43,28 @@ searchBtn.on('click', function(event) {
         return response.json();
       })
       .then(function(data) {
+        console.log(data);
+        localStorage.setItem("lat/lon: " + Math.round(data[0].lat) + '/' + Math.round(data[0].lon), data[0].name);
         getWeather(data[0].lat, data[0].lon);
       });
     var history = $('#history');
-    var cityBtn = $('<button>');
+    var cityBtn = $("<button>");
     capital = city.val().charAt(0).toUpperCase() + city.val().slice(1);
     cityBtn.addClass("list-group-item list-group-item-action");
     $('#name').text(capital + ' ' + moment().format("L"));
     cityBtn.text(capital);
     history.append(cityBtn);
     city.val('');
+    cityBtn.on('click', function() {
+      var url = 'http://api.openweathermap.org/geo/1.0/direct?q=' + this.textContent + '&appid=4013a002f8318887eb591e193340e547';
+      $('#name').text(this.textContent + ' ' + moment().format("L"));
+      fetch(url)
+        .then(function(response2) {
+          return response2.json();
+        })
+        .then(function(data2) {
+          getWeather(data2[0].lat, data2[0].lon);
+        });
+    })
 });
 
